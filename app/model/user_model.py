f241b3c8ae8d489b8d852232d138c090e8b069d0
Ashfak273 <mzashfak@gmail.com>
2024-07-30 22:26:18 +0530

    Final code

--- app/model/user_model.py
+++ app/model/user_model.py
@@ -1,30 +1,102 @@
+"""
+    User Models
+
+    This module contains the Pydantic models for User related operations.
+
+    Author
+    ----------
+    name: Ashfak Ahamed
+    email: mzashfak@gmail.com
+
+    Developers
+    ----------
+    - name: Ashfak Ahamed
+    email: mzashfak@gmail.com
+
+"""
 from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 
 
 class UserRegister(BaseModel):
+    """
+    UserRegister Model
+
+    Attributes:
+    ----------
+    username : str
+        The username of the user
+    email : EmailStr
+        The email of the user
+    password : str
+        The password of the user
+    """
     username: str
     email: EmailStr
     password: str
 
 
 class UserLogin(BaseModel):
+    """
+    UserLogin Model
+
+    Attributes:
+    ----------
+    username : str
+        The username of the user
+    password : str
+        The password of the user
+    """
     username: str
     password: str
 
 
 class AuthResponse(BaseModel):
-    access_token: Optional[str] = Field(None)
-    token_type: Optional[str] = Field(None)
-    success: bool = Field(...)
-    message: Optional[str] = Field(None)
+    """
+    AuthResponse Model
+
+    Attributes:
+    ----------
+    access_token : Optional[str]
+        The access token of the user
+    token_type : Optional[str]
+        The type of the token
+    success : bool
+        The success status of the authentication
+    message : Optional[str]
+        The message of the authentication response
+    """
+    access_token: Optional[str] = None
+    token_type: Optional[str] = None
+    success: bool
+    message: Optional[str] = None
 
 
 class UserUpdate(BaseModel):
-    username: Optional[str] = Field(None)
-    email: Optional[str] = Field(None)
-    password: Optional[str] = Field(None)
+    """
+    UserUpdate Model
+
+    Attributes:
+    ----------
+    username : Optional[str]
+        The username of the user
+    email : Optional[str]
+        The email of the user
+    password : Optional[str]
+        The password of the user
+    """
+    username: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
 
 
 class UserDelete(BaseModel):
+    """
+    UserDelete Model
+
+    Attributes:
+    ----------
+    username : str
+        The username of the user
+    """
     username: str
--- app/repository/user_repository.py
+++ app/repository/user_repository.py
@@ -1,3 +1,19 @@
+"""
+    User Repository
+
+    This module contains the UserRepository class for handling database operations related to the User entity.
+
+    Author
+    ----------
+    name: Ashfak Ahamed
+    email: mzashfak@gmail.com
+
+    Developers
+    ----------
+    - name: Ashfak Ahamed
+    email: mzashfak@gmail.com
+
+"""
 from sqlalchemy.orm import Session
 
 from app.entity.user_entity import User
@@ -5,9 +21,30 @@ from app.exceptions.db_operation_exception import DbOperationException
 
 
 class UserRepository:
+    """
+    UserRepository Class
+
+    This class is used to handle database operations related to the User entity such as getting user by username,
+    creating a user, getting user by id, updating a user, and deleting a user by id.
+
+    """
 
     @staticmethod
     def get_user_by_username(db: Session, username: str):
+        """
+        Get a user by username.
+
+        Parameters:
+        ----------
+        db : Session
+            The database session
+        username : str
+            The username of the user
+
+        Returns:
+        ----------
+        The user associated with the username
+        """
         try:
             return db.query(User).filter(User.username == username).first()
         except Exception as e:
@@ -15,6 +52,20 @@ class UserRepository:
 
     @staticmethod
     def create_user(db: Session, user: User):
+        """
+        Create a new user.
+
+        Parameters:
+        ----------
+        db : Session
+            The database session
+        user : User
+            The user data
+
+        Returns:
+        ----------
+        The created user
+        """
         try:
             db.add(user)
             db.commit()
@@ -26,6 +77,20 @@ class UserRepository:
 
     @staticmethod
     def get_user_by_id(db: Session, user_id: int):
+        """
+        Get a user by id.
+
+        Parameters:
+        ----------
+        db : Session
+            The database session
+        user_id : int
+            The id of the user
+
+        Returns:
+        ----------
+        The user associated with the id
+        """
         try:
             return db.query(User).filter(User.id == user_id).first()
         except Exception as e:
@@ -33,6 +98,22 @@ class UserRepository:
 
     @staticmethod
     def update_user(db: Session, user: User, user_update):
+        """
+        Update a user.
+
+        Parameters:
+        ----------
+        db : Session
+            The database session
+        user : User
+            The current user data
+        user_update : UserUpdate
+            The user update data
+
+        Returns:
+        ----------
+        The updated user
+        """
         try:
             if user_update.username:
                 user.username = user_update.username
@@ -48,6 +129,20 @@ class UserRepository:
 
     @staticmethod
     def delete_user_by_id(db: Session, user_id: int):
+        """
+        Delete a user by id.
+
+        Parameters:
+        ----------
+        db : Session
+            The database session
+        user_id : int
+            The id of the user
+
+        Returns:
+        ----------
+        None
+        """
         try:
             user = db.query(User).filter(User.id == user_id).first()
             db.delete(user)
